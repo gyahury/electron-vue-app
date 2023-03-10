629c24211bcc5d9e3c99c79525b427994e4888ed
gyahury <rudgns1243@naver.com>
2023-03-10 18:03:20 +0900

    file input frame added

--- src/components/FileNameList.vue
+++ src/components/FileNameList.vue
@@ -1,3 +1,17 @@
 <template>
     <p>여기는 파일이름리스트</p>
+
+    <v-file-input webkitdirectory></v-file-input>
+
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+        folder: null
+    }
+  }
+}
+  </script>
